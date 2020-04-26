b54164706a88f44641b2856f5fa0ac867d6dbf24
John Cribb <johncribb1@gmail.com>
2020-04-26 16:33:03 +0100

    Third function completed. Little help used from solution.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -20,6 +20,7 @@ def find(word_phrase, text):
 
 proprietary_terms = ["she",'she', "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+punctuation = [",", "!", "?", ".", "%", "/", "(", ")"]
 
 def find_ver2(text):
     working_text = text
@@ -28,24 +29,44 @@ def find_ver2(text):
             working_text = working_text.replace(i, '*Censored Information*')
     return working_text
     
-email2_censor = find_ver2(email_two)
-print(email2_censor)
-print(email_two)
+#email2_censor = find_ver2(email_two)
+#print(email2_censor)
+#print(email_two)
 
 def find_ver3(text):
     working_text = text
     for i in proprietary_terms:
         if i in working_text:
             working_text = working_text.replace(i, '*Censored Information*')
-    for bad in negative_words:
-        count = 0
-        if working_text.count(bad) >= 2:
-            count + working_text.count(bad)
-            working_text.replace(bad, '')
-            return count 
+    return working_text
+    
+def negative_count(text):
+    
+    working_text = []
+    
+    for x in text.split(" "):
+        x1 = x.split("\n")
+        for word in x1:
+            working_text.append(word)
+    
+    count = 0
+    for i in range(0,len(working_text)):
+      if (working_text[i] in negative_words) == True:
+        count += 1
+        if count > 2:
+          word_clean = working_text[i]
+          for x in punctuation:
+            word_clean = word_clean.strip(x)
+          censored_word = ""
+          for x in range(0,len(word_clean)):
+            censored_word = censored_word + "X"
+          working_text[i] = working_text[i].replace(word_clean, censored_word)
+    return " ".join(working_text)
+    
+
+email3_term_remove = find_ver3(email_three)
+email3_censor = negative_count(email3_term_remove)
+print(email3_censor)
 
 
-#print(email_three)
-#email3_censor = find_ver3(email_three)
-#print(email3_censor)
 
